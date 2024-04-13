57c30254b7aa07743899e0381256312ef012daea
ADUMAPATHI <adumapathi101@gmail.com>
2024-04-13 09:58:37 +0530

    Update settings.py

--- BlogDetails/settings.py
+++ BlogDetails/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-a3@2p(np&s61z7bwlpp@xi7f*x080i8bix4pe2x2bc7&gwa6-+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
