5d736b1e3ffb22b5795f4e9b43744bda50dd6f2b
ADUMAPATHI <adumapathi101@gmail.com>
2024-04-14 19:32:56 +0530

    all

--- Blog/views.py
+++ Blog/views.py
@@ -12,7 +12,6 @@ from rest_framework.response import Response
 from django.db.models import Count
 from rest_framework.decorators import api_view
 
-@api_view(["GET"])
 def login(request):
     if request.method == 'POST':
         form = UserLogForm(request.POST)
--- BlogDetails/settings.py
+++ BlogDetails/settings.py
@@ -30,7 +30,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Application definition
 
@@ -42,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Blog',
-    'rest_framework',
 ]
 
 MIDDLEWARE = [
